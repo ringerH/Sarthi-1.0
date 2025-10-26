085721564f54b7a6f28e9bd0c167e9284ab31da4
ringerH <delahuntim@gmail.com>
2025-10-26 20:14:13 +0530

    services updation

--- services/api-gateway/server.js
+++ services/api-gateway/server.js
@@ -4,13 +4,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const PORT = 3000;
 
-// Route /api/auth requests to the auth-service
 app.use('/api/auth', createProxyMiddleware({
     target: 'http://auth-service:5001',
     changeOrigin: true,
     pathRewrite: { '^/api/auth': '/' },
 }));
 
-// TODO: Add routes for other services here in the future
+app.use('/api/rides', createProxyMiddleware({
+    target: 'http://ride-service:5002',
+    changeOrigin: true,
+    pathRewrite: { '^/api/rides': '/' },
+}));
 
-app.listen(PORT, () => console.log(`🚀 API Gateway on port ${PORT}`));
+
+
+app.listen(PORT, () => console.log(`API Gateway on port ${PORT}`));
